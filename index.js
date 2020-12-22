555c7bdf58b34fa3fbf52ff5c2dbd872b049a22a
Ahmad Rulim <mfalkia342@gmail.com>
2020-12-22 23:17:25 +0700

    Initial commit
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

---
+++ index.js
@@ -0,0 +1,39 @@
+import sendTelegramNotification from './src/handlers/send_telegram_notification';
+import handleTelegramBot from './src/handlers/handle_telegram_bot';
+import Router from './src/utils/router';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  let response = '';
+
+  if (request.method == 'POST') {
+    const r = new Router();
+    // API_KEY format: string <api_key>
+    const apiKey = API_KEY;
+    r.post(`/sendTelegramNotification${apiKey}`, sendTelegramNotification);
+    r.post(`/handleTelegramBot${apiKey}`, handleTelegramBot);
+
+    response = await r.route(request);
+
+    if (response == 'undefined') {
+      const responseBody = JSON.stringify({
+        result: 'Failed',
+        data: 'Method not found!',
+      });
+      response = new Response(responseBody, {
+        status: 404,
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          'User-Agent': 'KweeksHook/1.0 (+https://webhook.kweeksnews.com/)',
+        }),
+      });
+    }
+  } else {
+    response = await fetch(request);
+  }
+
+  return response;
+}
---
+++ src/utils/telegramBot.js
@@ -0,0 +1,30 @@
+// TELEGRAM_TOKEN format: string <telegram_bot_token>
+const token = TELEGRAM_TOKEN;
+const telegramUrl = `https://api.telegram.org/bot${token}/`;
+
+export async function telegramBot(method, params) {
+  const fetchUrl = telegramUrl + method;
+
+  const fetchBody = JSON.stringify(
+    {
+      chat_id: params.chatId,
+      reply_to_message_id: params.messageId,
+      text: params.text,
+      reply_markup: params.replyMarkup,
+      parse_mode: params.parseMode,
+    },
+    (key, value) => {
+      if (value !== null && value !== undefined && value !== '') return value;
+    }
+  );
+
+  const fetchPayload = {
+    method: 'POST',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    body: fetchBody,
+  };
+
+  const fetchResult = await fetch(fetchUrl, fetchPayload);
+
+  return fetchResult.json();
+}
